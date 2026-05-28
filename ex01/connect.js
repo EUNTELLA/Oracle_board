9f9f01d211567606954a8e91f059caac55a0f9fa
EUNTELLA <yokohamay0612@gmail.com>
2026-05-28 15:01:36 +0900

    feat: update board for Oracle 21c

--- ex01/connect.js
+++ ex01/connect.js
@@ -1,11 +1,5 @@
 const oracledb = require('oracledb');
 
-try {
-    oracledb.initOracleClient(); // 환경변수 PATH에 Oracle 클라이언트가 등록되어 있지 않다면 { libDir: 'C:\\oracle\\instantclient_19_21' } 와 같이 경로를 명시해야 합니다.
-} catch (err) {
-    console.error('Oracle Client 초기화 실패:', err);
-}
-
 async function getConnection() {
     let connection;
     try {
